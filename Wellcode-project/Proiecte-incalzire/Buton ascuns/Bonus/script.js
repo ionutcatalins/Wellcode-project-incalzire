4c49146dda7458285c3fa2b3d29c4fe0f0e31daf
ionutcatalins <stanciuionutcatalin95@gmail.com>
2023-06-13 09:40:44 +0300

    Add all my files

--- Wellcode-project/Proiecte-incalzire/Buton ascuns/Bonus/script.js
+++ Wellcode-project/Proiecte-incalzire/Buton ascuns/Bonus/script.js
@@ -1,31 +1,33 @@
+let numberButton = 0;
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-let buttonWin = getRandomInt(3);
-
-function winButtonFirst() {    
-    if(buttonWin == 0) {
-        document.getElementById("text").innerHTML = 'Castigator'
-    } else {
-        document.getElementById("text").innerHTML = 'Necastigator'
+function button() {
+   let value = document.getElementById("btncount").value
+   let buttonWin = getRandomInt(value)
+   console.log(buttonWin)
+   for (let i = 0; i < value; ++i) {
+    let choose = 'loser()'
+        if (numberButton == buttonWin) {
+            choose = 'win()'
+        }  
+    ++numberButton
+        let newBtn = document.createElement(type='button');
+        newBtn.id = numberButton;
+         
+        newBtn.setAttribute("onclick", choose)
+        newBtn.innerText="Buton" + (numberButton)
+        document.querySelector('#result').appendChild(newBtn)
     }
     
 }
 
-function winButtonSecond() {    
-    if(buttonWin == 1) {
-        document.getElementById("text").innerHTML = 'Castigator'
-    } else {
-        document.getElementById("text").innerHTML = 'Necastigator'
-    }
-}
 
-function winButtonLast() {    
-    if(buttonWin == 2) {
-        document.getElementById("text").innerHTML = 'Castigator'
-    } else {
-        document.getElementById("text").innerHTML = 'Necastigator'
-    }
+function win() {
+    console.log('Castigator')
 }
-
+function loser() {
+    console.log('Necastigator')
+}

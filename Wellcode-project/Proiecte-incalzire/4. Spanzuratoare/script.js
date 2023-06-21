09663ac632d294eba0646223c57f673f858f19c5
ionutcatalins <stanciuionutcatalin95@gmail.com>
2023-06-21 13:49:55 +0300

    Add all my files

--- Wellcode-project/Proiecte-incalzire/4. Spanzuratoare/script.js
+++ Wellcode-project/Proiecte-incalzire/4. Spanzuratoare/script.js
@@ -1,16 +1,60 @@
+    var wordReceived;
+    var lengthString;
+    let lifee = 7;
+    let counterLetter
 
-
-function word() {
-    let wordReceived = document.getElementById("in").value
+function word() { // sohw word hidden and change button function 
+    wordReceived = document.getElementById("in").value
     document.getElementById("in").value = ""
-    let lengthString = wordReceived.length
-    console.log(lengthString)
-    $("#words").append(wordReceived[0]);
-    document.getElementById("words")
-    for(let i = 1; i < lengthString - 1; ++i) {
-       let a = document.createElement(<type="text" id="lengthString"> _ </text>"
-       a.id = lengthString
-       document.querySelector('#words').appendChild(a)
+    lengthString = wordReceived.length
+   
+    for (let i = 0; i < lengthString; ++i) {
+        let generate = document.createElement(type="text")
+        generate.id = i;
+        
+        if(i == 0) {
+            $('#words').append('<text id=' + i +' > ' + wordReceived[i] + ' ' + '</text>');
+        } else if (i < lengthString - 1) {
+            $('#words').append('<text id=' + i +'> _</text>');
+        } else if (i == lengthString - 1) {
+            $('#words').append('<text id=' + i +' > ' + wordReceived[i] + '</text>');
+        }
     }
-    $("#words").append(wordReceived[lengthString - 1]);
+   
+    counterLetter = lengthString - 2;
+    document.getElementById("btn").innerHTML = "Incearca litera";
+    document.getElementById("btn").setAttribute("onclick","letter()");
+    document.getElementById("life").innerHTML = "Numar vieti: " + lifee;
+}
+
+
+function letter() { // check every letter and letter completion
+    let letter = document.getElementById("in").value;
+    let lengthLetter = letter.length;
+    if(letter == "" || lengthLetter != 1) {
+        alert("Nu ati introdus litera sau ati introdus mai mult de o litera");
+    } else {
+        let check = 0;   
+        document.getElementById("in").value = ""  
+        for (let i = 1; i < lengthString - 1; ++i) {
+            if (letter == wordReceived[i]) {
+                document.getElementById(i).innerHTML = letter + " "
+                check = 1;
+                --counterLetter;
+            }          
+        }  
+        if (check == 0) {
+            --lifee;
+        }
+        
+        if (counterLetter == 0) {
+            alert("Ati castigat: " + wordReceived)
+            window.location.reload();
+        }
+        if (lifee == 0) {
+            alert("Ati pierdut cuvantul este: " + wordReceived)
+            window.location.reload();
+        }
+        document.getElementById("life").innerHTML = "Numar vieti: " + lifee;
+    }
 }
